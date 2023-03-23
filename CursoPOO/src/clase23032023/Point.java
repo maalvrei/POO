74acbea832907d86d273353e019941b18e8afead
maalvrei <miguemorisco@gmail.com>
2023-03-23 17:38:50 +0100

    point

--- CursoPOO/src/clase23032023/Point.java
+++ CursoPOO/src/clase23032023/Point.java
@@ -58,52 +58,68 @@ public class Point {
 
 	boolean up() {
 		if (y + 1 <= 7) {
-			y +=1;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	boolean down() {
-		if (y - 1 >=0) {
-			y -=1;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	boolean left() {
-		if (x - 1 >=0) {
-			x -=1;
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	boolean right() {
-		if (x + 1 >=0) {
-			x +=1;
+			y += 1;
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public static void main(String[] args) {
-		Point p1 = new Point();
-		Point p2 = new Point("green", 'a');
-		p2.showSimple();
-		for (int i = 7; i>=0;i--) {
-			if (i==0)
-				System.out.println(i + " +--+--+--+--+--+--+--+--+--+--+--+");
-				else
-			System.out.println(i + " +--+--+--+--+--+--+--+--+--+--+--+\n  |  |  |  |  |  |  |  |  |  |  |  |");
-			;
+	boolean down() {
+		if (y - 1 >= 0) {
+			y -= 1;
+			return true;
+		} else {
+			return false;
 		}
-		System.out.println("  0  1  2  3  4  5  6  7  8  9 10 11 12");
-		;
+	}
+
+	boolean left() {
+		if (x - 1 >= 0) {
+			x -= 1;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	boolean right() {
+		if (x + 1 >= 0) {
+			x += 1;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	void show() {
+		for (int i = 7; i >= 0; i--) {
+			for (int e = 0; e < 13; e++) {
+				if (e == 0 && y == 0 && i == x) {
+					System.out.print(i + " " + symbol);
+				} else {
+					if (e == 0) {
+						System.out.print(i + " +");
+					} else {
+						if (x == e) {
+							System.out.print("---" + symbol);
+						} else {
+							System.out.print("---+");
+						}
+					}
+					if (e == 12 && i <= 7) {
+						System.out.print("\n");
+						System.out.println("  |   |   |   |   |   |   |   |   |   |   |   |   |");
+					}
+				}
+			}
+		}
+		System.out.println("  0   1   2   3   4   5   6   7   8   9   10  11  12");
+	}
+
+	public static void main(String[] args) {
+		Point p1 = new Point((byte) 2, (byte) 0, "green", 'b');
+		Point p2 = new Point("green", 'a');
+		p1.show();
 	}
 }
